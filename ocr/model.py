0152d8f09708055c291297d83d8d76d9f3e9a41d
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2024-08-14 18:08:27 -0700

    Fix OCR module path issues

--- ocr/model.py
+++ ocr/model.py
@@ -16,10 +16,10 @@ limitations under the License.
 
 import torch.nn as nn
 
-from modules.transformation import TPS_SpatialTransformerNetwork
-from modules.feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
-from modules.sequence_modeling import BidirectionalLSTM
-from modules.prediction import Attention
+from ocr.modules.transformation import TPS_SpatialTransformerNetwork
+from ocr.modules.feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
+from ocr.modules.sequence_modeling import BidirectionalLSTM
+from ocr.modules.prediction import Attention
 
 
 class Model(nn.Module):
